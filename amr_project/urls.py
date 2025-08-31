e17980f74f404c9ecf254d8bed18cb4fc6a2d481
Harry Thabo Masuku <biomasuku@gmail.com>
2025-08-31 20:03:31 +0200

    Add clean / and /healthz JSON endpoints

--- amr_project/urls.py
+++ amr_project/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
-from .views import index, healthz
 from django.urls import path, include
+from .views import index, healthz
 
 urlpatterns = [
-    path('', index, name='index'),
     path('healthz', healthz, name='healthz'),
-    path('admin/', admin.site.urls),
-    # include amr_reports routes (adds /api/data-entry/ and /api/data-entry/bulk/)
+    path('', index, name='index'),
+
+    # API app
     path('', include('amr_reports.urls')),
+
+    # Admin (optional)
+    path('admin/', admin.site.urls),
 ]
