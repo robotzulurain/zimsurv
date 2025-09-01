cc5acbcc195d1ccf1b9307570889ac00d7bb3a1c
Harry Thabo Masuku <biomasuku@gmail.com>
2025-09-01 10:45:13 +0200

    Fix CORS middleware (clean headers + Vary handling)

--- amr_project/mw_cors_simple.py
+++ amr_project/mw_cors_simple.py
@@ -10,27 +10,28 @@ class SimpleCORSMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
+        origin = request.headers.get("Origin")
+
         # Preflight short-circuit
         if request.method == "OPTIONS":
-            origin = request.headers.get("Origin")
             resp = HttpResponse()
             resp["Access-Control-Allow-Origin"] = origin or "*"
             resp["Access-Control-Allow-Methods"] = "GET, POST, PUT, PATCH, DELETE, OPTIONS"
             resp["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-Requested-With"
             resp["Access-Control-Max-Age"] = "86400"
             if origin:
-                # Vary so caches don't mix origins
                 resp["Vary"] = "Origin"
             return resp
 
-        # Normal request -> get downstream response, then add CORS
+        # Normal request -> call the view, then add CORS headers
         response = self.get_response(request)
-        origin = request.headers.get("Origin")
         response["Access-Control-Allow-Origin"] = origin or "*"
         response["Access-Control-Allow-Methods"] = "GET, POST, PUT, PATCH, DELETE, OPTIONS"
         response["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-Requested-With"
-        # If you need cookies one day:
+        # If you ever need cookies, also set:
         # response["Access-Control-Allow-Credentials"] = "true"
         if origin:
-            response["Vary"] = (response.get("Vary", "") + ", Origin").strip(", ").replace(",,", ",")
+            existing_vary = response.get("Vary", "")
+            if "Origin" not in existing_vary:
+                response["Vary"] = (existing_vary + (", " if existing_vary else "") + "Origin")
         return response
